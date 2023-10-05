5a4b4e2b157398f9dfcb6d467e0700cf3fb2915f
Mizpuno <nititorn.certihunt@gmail.com>
2023-10-05 09:40:33 +0700

    new commit

--- test_function.js
+++ test_function.js
@@ -1,24 +1,29 @@
 const {helloworld, oneplus } = require("./Utils")
+const fs = require('fs')
+let str = ''
 
 if (helloworld() === "hello world") {
 
 } else {
+    str = str + "error at: helloworld(): at 1"
     console.log(1)
-    return
 }
 
 // if 1+1 == 2 is true
 if (oneplus(1) === 1) {
 
 } else {
+    str += "error at: oneplus(1) <-- 1 --> must be 2!"
     console.log(1)
-    return
 }
 
 // if 2+1 == 3 is true
 if (oneplus(2) === 3) {
 
 } else {
+    str += "error at: oneplus(2) <-- 2 --> must be 3!"
     console.log(1)
     return
 }
+
+fs.writeFileSync('log.txt', str)
